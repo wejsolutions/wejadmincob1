ad409f32f5c7bdde06926be87af8714f314c4555
wejsolutions <wejsolutions@gmail.com>
2022-07-02 18:35:39 -0400

    cambio hecho por jose

--- borrar.php
+++ borrar.php
@@ -1,5 +1,5 @@
 <?php
-echo "Prueba 6. Moficado desde Equipo de Edgar";
-echo "\n Hola mundo por Jose Gil";
-echo "Prueba 8 desde william";
+	echo "Prueba 6. Moficado desde Equipo de Edgar";
+	echo "\n Hola mundo por Jose Gil";
+	echo "Prueba 8 desde william";
 ?>
